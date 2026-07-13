63311a0aa7eaa676ff8276374e56ac977a46772f
life1342 <bertrand.life1342@gmail.com>
2026-07-13 12:16:36 +0000

    mise en place des wrapper et les notification avec react-toastify

---
+++ app/components/Wrapper.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { ToastContainer } from 'react-toastify'
+
+type WrapperPros = {
+    children : React.ReactNode
+}
+
+const Wrapper = ({children} : WrapperPros) => {
+  return (
+    <div>
+        
+       {/*<NavBar />*/}
+       <ToastContainer position='top-center'  autoClose={10000} hideProgressBar={false} newestOnTop={false}
+       closeOnClick draggable pauseOnHover/>
+       <div className='px-5 md:px-[10%] mt-8 mb-10'>
+        {children}
+       </div>
+    </div>
+  )
+}
+
+export default Wrapper
