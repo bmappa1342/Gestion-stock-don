1b56ea1f7ea13cfe813642a9e6cd29037a03829b
life1342 <bertrand.life1342@gmail.com>
2026-07-20 10:30:42 +0000

    mise en place des api front end  pour la categorie

--- app/categories/page.tsx
+++ app/categories/page.tsx
@@ -1,14 +1,142 @@
 "use client"
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Wrapper from '../components/Wrapper'
-
-
+import CategoryModal from '../components/CategoryModal'
+import { useUser } from '@clerk/nextjs'
+import { createCategory, deleteCategory, readCategories, updateCategory } from '../actions'
+import { toast } from 'react-toastify'
+import { Category } from '@prisma/client'
+import EmptyState from '../components/EmptyState'
+import { Pencil, Trash } from 'lucide-react'
 
 const page = () => {
+  const { user } = useUser()
+  const email = user?.primaryEmailAddress?.emailAddress as string
+
+  const [name, setName] = useState("")
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  const [description, setDescription] = useState("")
+  const [loading, setLoading] = useState(false)
+  const [editMode, setEditMode] = useState(false)
+  const [editingCategoryId, setEditingCategoryId] = useState<string | null>(null)
+  const [categories, setCategories] = useState<Category[]>([])
+
+  const loadCategories = async () => {
+    if (email) {
+      const data = await readCategories(email)
+      if (data)
+        setCategories(data)
+    }
+  }
+  useEffect(() => {
+    loadCategories()
+  }, [email])
+
+  const openCreateModal = () => {
+    setName("");
+    setDescription("");
+    setEditMode(false);
+    (document.getElementById("category_modal") as HTMLDialogElement)?.showModal()
+  }
+
+  const closeModal = () => {
+    setName("");
+    setDescription("");
+    setEditMode(false);
+    (document.getElementById("category_modal") as HTMLDialogElement)?.close()
+  }
+
+  const handleCreateCategory = async () => {
+    setLoading(true)
+    if (email) {
+      await createCategory(name, email, description)
+    }
+    await loadCategories()
+    closeModal()
+    setLoading(false)
+    toast.success("Category create successful.")
+  }
+
+  const handleUpdateCategory = async () => {
+    if (!editingCategoryId) return
+    setLoading(true)
+    if (email) {
+      await updateCategory(editingCategoryId, email, name, description)
+    }
+    await loadCategories()
+    closeModal()
+    setLoading(false)
+    toast.success("Category edit successful.")
+  }
+
+  const openEditModal = (category: Category) => {
+    setName(category.name);
+    setDescription(category.description || " ");
+    setEditMode(true);
+    setEditingCategoryId(category.id);
+    (document.getElementById("category_modal") as HTMLDialogElement)?.showModal()
+  }
+
+
+  const handleDeleteCategory = async (categoryId: string) => {
+    const confirmDelete = confirm("Are you sure you want to delete this category? All associated products will be deleted as well")
+    if (!confirmDelete) return;
+    await deleteCategory(categoryId, email);
+    await loadCategories();
+    toast.success("Category deleted successful.")
+  }
+
+
   return (
     <Wrapper>
-        
-        test
+
+      <div>
+        <div className='mb-4'>
+          <button className='btn btn-primary'
+            onClick={openCreateModal}
+          >
+           Add Category
+          </button>
+        </div>
+
+
+        {categories.length > 0 ? (
+          <div>
+            {categories.map((category) => (
+              <div key={category.id} className='mb-2 p-5 border-2 border-base-200 rounded-3xl flex justify-between items-center'>
+                <div>
+                  <strong className='text-lg'>{category.name}</strong>
+                  <div className='text-sm'>{category.description}</div>
+                </div>
+                <div className='flex gap-2'>
+                  <button className='btn btn-sm' onClick={() => openEditModal(category)}>
+                    <Pencil className='w-4 h-4' />
+                  </button>
+                  <button className='btn btn-sm btn-error' onClick={() => handleDeleteCategory(category.id)}>
+                    <Trash className='w-4 h-4' />
+                  </button>
+                </div>
+              </div>
+            ))}
+          </div>
+        ) : (
+          <EmptyState
+            message={"No category"}
+            IconComponent='Group'
+          />
+        )}
+      </div>
+
+      <CategoryModal
+        name={name}
+        description={description}
+        loading={loading}
+        onclose={closeModal}
+        onChangeName={setName}
+        onChangeDescription={setDescription}
+        onSubmit={editMode ? handleUpdateCategory : handleCreateCategory}
+        editMode={editMode}
+      />
 
     </Wrapper>
   )
